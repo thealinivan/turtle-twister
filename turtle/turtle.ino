69c91ed177b4ee0d4e754ad503f0a506096dfc51
alinivan <ai626@live.mdx.ac.uk>
2021-11-22 13:19:16 +0000

    added defined methods for encoder counts and direction

--- turtle/turtle.ino
+++ turtle/turtle.ino
@@ -8,15 +8,23 @@ struct velocity {
   struct linear {float x = 0.05; float y = 0; float z = 0;};
   struct angular {float x = 0; float y = 0; float z = 3;};
 };
-double linearX = 0.5;
-double angularZ = 3;
+const double linearX = 0.5;
+const double angularZ = 3;
+
+//motors
+//right
+const int AIN1 = 5;  // acw
+const int AIN2 = 10; // cw
+//left
+const int BIN1 = 6;  // cw
+const int BIN2 = 9;  // acw
 
 //pid - variables related to pid
 double leftMotorSetPoint, leftMotorInput, leftMotorOutput;
 double rightMotorSetPoint, rightMotorInput, rightMotorOutput;
-int Kp=.5, Ki=.5, Kd=.5;
-PID leftMotorPID(leftMotorSetPoint, leftMotorInput, leftMotorOutput, Kp, Ki, Kd, DIRECT);
-PID rightMotorPID(rightMotorSetPoint, rightMotorInput, rightMotorOutput, Kp, Ki, Kd, DIRECT);
+const int Kp=1, Ki=1, Kd=1;
+PID leftMotorPID(&leftMotorSetPoint, &leftMotorInput, &leftMotorOutput, Kp, Ki, Kd, DIRECT);
+PID rightMotorPID(&rightMotorSetPoint, &rightMotorInput, &rightMotorOutput, Kp, Ki, Kd, DIRECT);
 
 //interrupts
 
@@ -26,7 +34,7 @@ PID rightMotorPID(rightMotorSetPoint, rightMotorInput, rightMotorOutput, Kp, Ki,
 
 //motor rotation direction
 
-
+//SETUP
 void setup() {
   //print
   Serial.begin(9600);
@@ -38,20 +46,31 @@ void setup() {
   
 }
 
+//LOOP
 void loop() {
+  analogWrite(AIN1, 0);
+  analogWrite(AIN2, 0);
+  
+  analogWrite(BIN1, 0);
+  analogWrite(BIN2, 0);
 }
 
-//ENCODER CONTS - return encoder counts
+//ENCODER CONTS
+// return motor encoder counts to reach speed in m/s
+double getMotorSpeedInEncoderCounts(double linear_x, double angular_z){
 
+}
 
-//MOTOR ROTATION DIRECTION - return rotation direction
-
-
-//PID - run a specific motor
-void runPID(int motor_encoder_counts, int motor_rotation_direction, PID motor_PID){
+//MOTOR ROTATION DIRECTION
+// return PWMA pin that sets direction (cw or acw)
+int getMotorRotationDirection (double linear_x, double angluar_z) {
   
 }
 
+//PID - run a specific motor
+void runMotor(int motor_encoder_counts, int motor_rotation_direction, PID motor_PID){
+  
+}
 
 //MAP DATA FOR LINEAR MOVEMENT
 //MAP DATA FOR ANGULAR MOVEMENT
