132f21a9bbb05021fd5d37b9c5993bb2ed128e2d
alinivan <ai626@live.mdx.ac.uk>
2021-11-22 20:04:13 +0000

    attempted PID, added differential movement with angular velocity

--- turtle/turtle.ino
+++ turtle/turtle.ino
@@ -3,76 +3,163 @@
 
 //GLOBAL DATA - variables that can be used in any method
 
-//twist message - access method (velocity.linear.x / velocity.angular.z)
-struct velocity {
-  struct linear {float x = 0.05; float y = 0; float z = 0;};
-  struct angular {float x = 0; float y = 0; float z = 3;};
-};
-const double linearX = 0.5;
-const double angularZ = 3;
+//twist message data
+double linearX = .2;
+double angularZ = -2;
 
 //motors
-//right
-const int AIN1 = 5;  // acw
-const int AIN2 = 10; // cw
-//left
-const int BIN1 = 6;  // cw
-const int BIN2 = 9;  // acw
+const int RIN1 = 5;  // R ACW
+const int RIN2 = 10; // R CW
+const int LIN1 = 6;  // L CW
+const int LIN2 = 9;  // L ACW
+
+//linear measurements
+const int ticksPerWheelRotation = 850;
+const double wheelCircumference = .207;
+const double angRadius = .17;
+const float Pi = 3.14159;
+const double circleCircumference = angRadius*Pi;
+const int maxEnC = 216;
+
+//encoders
+const int REN1 = 3; // R
+const int REN2 = 7; // R
+const int LEN1 = 4; // L
+const int LEN2 = 2; // L
+volatile int prevC = 0, count;
+unsigned long period = 100, nextT = period;
+int linearSpeedEncoderCount = 0; //ticks/100ms  .1m
+int leftMotorAngularSpeedEncoderCount = 0; //ticks/100ms .1rad
+int rightMotorAngularSpeedEncoderCount = 0; 
 
 //pid - variables related to pid
 double leftMotorSetPoint, leftMotorInput, leftMotorOutput;
 double rightMotorSetPoint, rightMotorInput, rightMotorOutput;
-const int Kp=1, Ki=1, Kd=1;
+const int Kp=5, Ki=3, Kd=3;
 PID leftMotorPID(&leftMotorSetPoint, &leftMotorInput, &leftMotorOutput, Kp, Ki, Kd, DIRECT);
 PID rightMotorPID(&rightMotorSetPoint, &rightMotorInput, &rightMotorOutput, Kp, Ki, Kd, DIRECT);
 
-//interrupts
-
-
-//encoder counts
-
-
-//motor rotation direction
-
 //SETUP
 void setup() {
+  if (angularZ > 0) {
+    leftMotorAngularSpeedEncoderCount = angularZ*(ticksPerWheelRotation*(circleCircumference/wheelCircumference)/(2*Pi))/10;
+  } else if (angularZ < 0) {
+    rightMotorAngularSpeedEncoderCount = -angularZ*(ticksPerWheelRotation*(circleCircumference/wheelCircumference)/(2*Pi))/10; 
+  } else {
+    leftMotorAngularSpeedEncoderCount = 0;
+    rightMotorAngularSpeedEncoderCount = 0;
+  }
   //print
   Serial.begin(9600);
+  //pins
+  pinMode(REN1, INPUT_PULLUP);
+  pinMode(REN2, INPUT_PULLUP);
+  pinMode(LEN2, INPUT_PULLUP);
+  pinMode(LEN2, INPUT_PULLUP);
   //pid - setup related to pid
+  rightMotorSetPoint = linearSpeedEncoderCount;
+  leftMotorSetPoint = linearSpeedEncoderCount;
+  rightMotorPID.SetTunings(Kp, Ki, Kd);
+  leftMotorPID.SetTunings(Kp, Ki, Kd);
+  rightMotorPID.SetMode(AUTOMATIC);
+  leftMotorPID.SetMode(AUTOMATIC);
   
   //encoder counts
-  
-  //motor rotation direction
-  
+  //interrups
+  attachInterrupt(0, interruptR, CHANGE);
+  attachInterrupt(1, interruptL, CHANGE); 
 }
 
 //LOOP
 void loop() {
-  analogWrite(AIN1, 0);
-  analogWrite(AIN2, 0);
-  
-  analogWrite(BIN1, 0);
-  analogWrite(BIN2, 0);
+  if (linearX > 0) {
+    linearSpeedEncoderCount = linearX*(ticksPerWheelRotation/wheelCircumference)/10; // ticks/100ms
+    moveForward();
+  } else if (linearX < 0) {
+      linearSpeedEncoderCount = -linearX*(ticksPerWheelRotation/wheelCircumference)/10; // ticks/100ms 
+      moveBackward(); 
+  } else {
+    stopTurtle();
+  }
+  getEncoderSpeed();
 }
 
-//ENCODER CONTS
-// return motor encoder counts to reach speed in m/s
-double getMotorSpeedInEncoderCounts(double linear_x, double angular_z){
-
+//INTERRUPTS
+// read pulse and update count
+void interruptR() { 
+      if (digitalRead(REN1)==digitalRead(REN2)) { --count; } 
+      else { ++count;}
+}
+void interruptL() { 
+      if (digitalRead(REN1)==digitalRead(REN2)) { ++count; } 
+      else { --count;}
 }
 
-//MOTOR ROTATION DIRECTION
-// return PWMA pin that sets direction (cw or acw)
-int getMotorRotationDirection (double linear_x, double angluar_z) {
-  
+//ENCODER COUNT
+// return ecoder speed ticks/100ms
+int getEncoderSpeed() {
+  unsigned long currT = millis();
+  if (currT >= nextT){
+      noInterrupts();
+      int currentCount = count;
+      interrupts();
+      int encoderSpeed = currentCount - prevC;  // ticks/100ms
+      prevC = currentCount;
+      nextT = currT + period;
+      Serial.println(encoderSpeed);
+      Serial.print(" "); 
+      return encoderSpeed;
+  } else {
+    return 0;
+  } 
 }
 
-//PID - run a specific motor
-void runMotor(int motor_encoder_counts, int motor_rotation_direction, PID motor_PID){
-  
-}
-
-//MAP DATA FOR LINEAR MOVEMENT
-//MAP DATA FOR ANGULAR MOVEMENT
 //RUN LEFT MOTOR
+ void leftMotor(char d){
+//  leftMotorInput = leftMotorSetPoint - getEncoderSpeed();
+//  leftMotorPID.Compute();
+  int ms = map(linearSpeedEncoderCount + leftMotorAngularSpeedEncoderCount/2 - rightMotorAngularSpeedEncoderCount/2, 0, maxEnC, 0, 255);
+  if (d=='L'){
+    analogWrite(LIN1, 0);
+    analogWrite(LIN2, ms);
+  } else if (d=='R') {
+    analogWrite(LIN1, ms);
+    analogWrite(LIN2, 0);
+  } else {
+    analogWrite(LIN1, 0);
+    analogWrite(LIN2, 0);
+  }
+ 
+}
+
 //RUN RIGHT MOTOR
+void rightMotor(char d){
+  int ms = map(linearSpeedEncoderCount - leftMotorAngularSpeedEncoderCount/2 + rightMotorAngularSpeedEncoderCount/2, 0, maxEnC, 0, 255);
+  if (d=='R'){
+    analogWrite(RIN1, 0);
+    analogWrite(RIN2, ms);
+  } else if (d=='L') {
+    analogWrite(RIN1, ms);
+    analogWrite(RIN2, 0);
+  } else {
+    analogWrite(RIN1, 0);
+    analogWrite(RIN2, 0);
+  }
+
+}
+
+//RUN BOTH MOTORS
+void moveForward(){
+  leftMotor('L');
+  rightMotor('R');
+}
+
+void moveBackward(){
+  leftMotor('R');
+  rightMotor('L');
+}
+
+void stopTurtle(){
+  leftMotor('stop');
+  rightMotor('stop');
+}
