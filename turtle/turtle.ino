c8dec446a814f1c51ee363cb2eb3e6019c81745e
alinivan <ai626@live.mdx.ac.uk>
2022-01-18 17:55:23 +0000

    added motor speed limits and solved angular speed error

--- turtle/turtle.ino
+++ turtle/turtle.ino
@@ -21,9 +21,10 @@ unsigned long nextTR = period;
 
 int currEncL = 0, currEncR = 0;
 
-int ECLinear = 0; // target ticks/100ms
-int ECAngL = 0; // target ticks/100ms
-int ECAngR = 0; // target ticks/100ms
+int ECLinear = 0; // target ticks/100ms linear speed
+int ECAngL = 0; // target ticks/100ms angular speed left motor
+int ECAngR = 0; // target ticks/100ms angular speed right motor
+int ECAngBoth = 0; // target ticks/100ms angular speed common for both motors
 
 // Linear measurements
 const int ticksPerWR = 852;
@@ -32,7 +33,7 @@ const double angDiameter = .165;
 const float Pi = 3.14159265259;
 const double circleCirc = angDiameter*Pi;
 const double minEncCount = 0;   // ticks/100 milliseconds
-const double maxEncCount = 213; // ticks/100 milliseconds
+const double maxEncCount = 216; // ticks/100 milliseconds
 
 
 // PID
@@ -58,35 +59,51 @@ void setup()
 // LOOP
 void loop()
 {
-  moveTurtle(.5, 0); 
-  // linear values between -0.5 and +0.5
-  // angular values between -5 and +5
+  moveTurtle(.4, 0); 
+}
+
+// Limit encoder counts
+void getEC(int ec){
+  if (ec > maxEncCount) { return maxEncCount; } 
+  else { return ec; }
 }
 
 // Move Turtle
 void moveTurtle(double linear_x, double angular_z) {
   setAngular(linear_x, angular_z);
-  setLinear(linear_x); 
+  setLinear(linear_x, angular_z); 
 }
 
 // Angular direction & velocity
 void setAngular(double lx, double az) {
-  int ECAng = az*(ticksPerWR*(circleCirc/wheelCirc)/(2*Pi))/10; 
+  int ECAng = az*((ticksPerWR*(circleCirc/wheelCirc)/(2*Pi)))/10; // ticks/100ms for az velocity
   if (az > 0) {
     if (lx >= 0){
       ECAngL = ECAng;
       ECAngR = 0;
-    } else {
+      ECAngBoth = 0;
+    } else if (lx < 0) {
       ECAngL = 0;
       ECAngR = ECAng;
+      ECAngBoth = 0;
+    } else if (lx == 0){
+      ECAngL = 0;
+      ECAngR = 0;
+      ECAngBoth = ECAng/2;
     }
   } else if (az < 0) {
-    if (lx >= 0) {
+    if (lx > 0) {
       ECAngL = 0;
       ECAngR = ECAng;
-    } else {
+      ECAngBoth = 0;
+    } else if (lx < 0) {
       ECAngL = ECAng;
       ECAngR = 0;
+      ECAngBoth = 0;
+    } else if (lx == 0){
+      ECAngL = 0;
+      ECAngR = 0;
+      ECAngBoth = ECAng/2;
     }
   } else {
     ECAngL = 0;
@@ -95,28 +112,36 @@ void setAngular(double lx, double az) {
 }
 
 // Linear direction &  velocity
-void setLinear(double lx){
-  ECLinear = lx*(ticksPerWR/wheelCirc)/10; // ticks/100 milliseconds
-  if (ECLinear >= 0) {
+void setLinear(double lx, double az){
+  ECLinear = lx*(ticksPerWR/wheelCirc)/10; // ticks/100ms for lx velocity
+  if (ECLinear > 0) {
     leftMotor('L');
     rightMotor('R');
   } else if (ECLinear < 0){
     leftMotor('R');
     rightMotor('L');
-  } else {
-     leftMotor('stop');
-     rightMotor('stop');
+  } else if (ECLinear == 0) {
+    if (az > 0){
+      leftMotor('L');
+      rightMotor('L');
+    } else if (az < 0) {
+      leftMotor('R');
+      rightMotor('R');
+    } else {
+      leftMotor("stop");
+      rightMotor("stop");
+    }
   }
 }
 
 // Left Motor
- void leftMotor(char d){
-  SetpointL = ECLinear + ECAngL/2 - ECAngR/2;
+ void leftMotor(char d){ 
+  SetpointL = ECLinear + ECAngL/2 - ECAngR/2 + ECAngBoth;
   InputL = getEncoderSpeed('L'); // 
   leftPID.Compute();
-  Serial.print(SetpointL); // the target speed (enconder counts / 100 milisenconds)
-  Serial.print(" ");
-  Serial.println(InputL); // current speed (enconder counts / 100 milisenconds)
+  //Serial.print(SetpointL); // the target speed (enconder counts / 100 milisenconds)
+  //Serial.print(" ");
+  //Serial.println(InputL); // current speed (enconder counts / 100 milisenconds)
   //Serial.print(" ");
   if (d=='L'){
     analogWrite(LIN1, 0);
@@ -132,12 +157,12 @@ void setLinear(double lx){
 
 // Right Motor
 void rightMotor(char d){
-  SetpointR = ECLinear - ECAngL/2 + ECAngR/2;
+  SetpointR = ECLinear - ECAngL/2 + ECAngR/2 + ECAngBoth;
   InputR = getEncoderSpeed('R');
   rightPID.Compute();
-  //Serial.print(SetpointR);
-  //Serial.print(" ");
-  //Serial.println(OutputR);
+  Serial.print(SetpointR);
+  Serial.print(" ");
+  Serial.println(OutputR);
   if (d=='R'){
     analogWrite(RIN1, 0);
     analogWrite(RIN2, OutputR);
